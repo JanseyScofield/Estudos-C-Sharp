08e2c47d0199556c9f77eba2feae6970d019017b
Jansey Scofield da Silva Filho <jsfilho@fapex.org.br>
2024-04-23 09:41:34 -0300

    Aula10.c#

--- Aula08/Aula08/obj/Release/net8.0/Aula08.AssemblyInfo.cs
+++ Aula08/Aula08/obj/Release/net8.0/Aula08.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula08")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625a41c48083e026e2ef89475c8de8bdf492d410")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula08")]
---
+++ Aula10/Aula10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Aula10 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um número: ");
+            double n = double.Parse(Console.ReadLine()) ;
+            double raizQuad;
+            while(n >= 0 ) {
+                raizQuad = Math.Sqrt(n) ;
+                Console.WriteLine($"A raiz de {n} é {raizQuad:F3}.");
+                Console.WriteLine("Digite outro número: ");
+                n = double.Parse(Console.ReadLine()) ;
+            }
+            Console.WriteLine("Número negativo!");
+        }
+    }
+}
