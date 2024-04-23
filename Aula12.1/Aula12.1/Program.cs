f0b896f293b716421c243b740e6c65880311bb0d
Jansey Scofield da Silva Filho <jsfilho@fapex.org.br>
2024-04-23 10:09:45 -0300

    Aula12_12.c#

---
+++ Aula12.1/Aula12.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Aula12_1 {
+    class Program {
+        static void Main(string[] args) {
+            double[] tri1 = new double[3];
+            double[] tri2 = new double[3];
+            double[] p = new double[2];
+            double[] area = new double[2];
+            int iCont;
+            Console.WriteLine("Entre com os lados do triângulo 1: ");
+            for (iCont = 0; iCont <= 2; iCont++) {
+                Console.WriteLine($"Lado {iCont + 1}:");
+                tri1[iCont] = double.Parse(Console.ReadLine());
+            }
+            p[0] = (tri1[0] + tri1[1] + tri1[2]) / 2.0;
+
+            Console.WriteLine("Entre com os lados do triângulo 2: ");
+            for (iCont = 0; iCont <= 2; iCont++) {
+                Console.WriteLine($"Lado {iCont + 1}:");
+                tri2[iCont] = double.Parse(Console.ReadLine());
+            }
+            p[1] = (tri2[0] + tri2[1] + tri2[2]) / 2.0;
+
+            area[0] = Math.Sqrt(p[0] * (p[0] - tri1[0]) * (p[0] - tri1[1]) * (p[0] - tri1[2]));
+            area[1] = Math.Sqrt(p[1] * (p[1] - tri2[0]) * (p[1] - tri2[1]) * (p[1] - tri2[2]));
+
+            Console.WriteLine($"A área do triângulo 1 é igual a {area[0]:F4} m².");
+            Console.WriteLine($"A área do triângulo 2 é igual a {area[1]:F4} m².");
+        }
+    }
+}
--- Aula12.1/Aula12.1/obj/Debug/net8.0/Aula12.1.AssemblyInfo.cs
+++ Aula12.1/Aula12.1/obj/Debug/net8.0/Aula12.1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula11")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula12.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d24540910a08d52cdc288f1df1edab4279860ba")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula11")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula11")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula12.1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula12.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
