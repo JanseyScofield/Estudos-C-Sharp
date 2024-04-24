db169df067f5710a5f7b07b4917eec5b92a5e39e
Jansey Scofield da Silva Filho <jsfilho@fapex.org.br>
2024-04-24 09:39:19 -0300

    Aula17.c#

---
+++ Aula17/Aula17/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula16 {
+    internal class Produto {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            _preco = preco;
+           _quantidade = quantidade;
+        }
+
+        public string GetNome() {
+            return _nome; 
+        }
+        
+        public void SetNome(string nome) {
+            if(nome != null && nome.Length > 1)
+                _nome = nome;
+        }
+
+        public double GetPreco() { 
+            return _preco;
+        }
+
+        public int GetQuantidade() {
+            return _quantidade;
+        }
+
+        public double ValorTotalEmEstoque() {
+            return _quantidade * _preco;
+        }
+
+        public void AdicionarProdutos(int numAdd) {
+            _quantidade += numAdd;
+        }
+
+        public void RemoverProdutos(int numRmv) {
+            _quantidade -= numRmv;
+        }
+
+        public override string ToString() {
+            return _nome + ", " + _quantidade + " unidades, valor do produto R$ " + _preco + ", Total: R$ " + ValorTotalEmEstoque();
+        }
+    }
+}
---
+++ Aula17/Aula17/Program.cs
@@ -0,0 +1,17 @@
+﻿using Aula16;
+using System;
+using System.Globalization;
+
+namespace Aula17 {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+
+        }
+    }
+}
