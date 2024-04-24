8b4529b156791334fb060e95cf61b56ffb7e661c
Jansey Scofield da Silva Filho <jsfilho@fapex.org.br>
2024-04-24 10:37:30 -0300

    ExerciciosPropostosP6.c#

---
+++ ExerciciosPropostosP6/ExerciciosPropostosP6/Conta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExerciciosPropostosP6 {
+    internal class Conta {
+        private int _numConta;
+        private string _nomeTitular;
+        private double _saldo;
+
+        public Conta(int numConta, string nomeTitular, double saldo) {
+            _nomeTitular = nomeTitular;
+            _numConta = numConta;
+            _saldo = saldo;
+        }
+
+        public void Deposito(double valor) {
+            _saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            _saldo -= valor+5;
+        }
+
+        public override string ToString() {
+            return "Conta " + _numConta + ", Ttiular: " + _nomeTitular + ", Saldo: R$ " + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExerciciosPropostosP6/ExerciciosPropostosP6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace ExerciciosPropostosP6 {
+    class Program {
+        public static void Main(string[] args) {
+            Console.Write("Entre o número da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            double depoInicial;
+            if (resposta == 's') {
+                Console.Write("Entre com o valor do depósito inicial:");
+                depoInicial = double.Parse(Console.ReadLine());
+            }
+            else {
+                depoInicial = 0.0;
+            }
+
+
+            Conta conta = new Conta(numConta, nomeTitular, depoInicial);
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: " + conta);
+
+            double movimentacoes;
+            Console.Write("Entre com o valor de depósito: ");
+            movimentacoes = double.Parse(Console.ReadLine());
+            conta.Deposito(movimentacoes);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + conta);
+            Console.WriteLine();
+            Console.Write("Entre com o valor de saque: ");
+            movimentacoes = double.Parse(Console.ReadLine());
+            conta.Saque(movimentacoes);
+            Console.WriteLine("Dados atualizados: " + conta);
+        }
+    }
+}
