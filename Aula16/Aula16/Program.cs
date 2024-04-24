a16b6bad747abeef1f8f31c7ae7efea1eeb1bdf9
Jansey Scofield da Silva Filho <jsfilho@fapex.org.br>
2024-04-24 09:07:32 -0300

    Aula16.c#

---
+++ Aula16/Aula16/Program.cs
@@ -0,0 +1,30 @@
+﻿using Aula16;
+using System;
+
+namespace Aula16 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Produto p1 = new Produto(nome, preco);
+
+            Console.WriteLine($"Dados do produto: {p1}");
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtdAdicionar = int.Parse(Console.ReadLine());
+            p1.AdicionarProdutos(qtdAdicionar);
+            Console.WriteLine($"Dados atualizados: {p1}");
+
+            Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+            int qtdRemover = int.Parse(Console.ReadLine());
+            p1.RemoverProdutos(qtdRemover);
+            Console.WriteLine($"Dados atualizados: {p1}");
+
+        }
+    }
+}
--- Aula16/Aula16/obj/Debug/net8.0/Aula16.AssemblyInfo.cs
+++ Aula16/Aula16/obj/Debug/net8.0/Aula16.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula15")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2c89bc56bb42add626ee0a79c0601f4f302dd3")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula15")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula15")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula16")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
